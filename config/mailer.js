372a7d19155a866dc21354cba8f54d0bba3eeb75
kimhab <kimhabsok9@gmail.com>
2026-05-19 16:09:22 +0700

    update

--- config/mailer.js
+++ config/mailer.js
@@ -4,8 +4,8 @@ const transpoter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     service: 'gmail',
     auth: {
-        user: "kimhabsok9@gmail.com",
-        pass: "yozkcxkhfnmyvtfu"
+        user: 'kimhabsok9@gmail.com',
+        pass: 'yozkcxkhfnmyvtfu'
     }
 });
 
